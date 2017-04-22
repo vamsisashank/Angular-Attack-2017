f6ce14b3ad0074bee7d2b33c62b20cc7c792b842
Sanket Tajane <sanket.tajane@gmail.com>
2017-04-22 13:30:49 -0500

    feat(angular-fire): added videos to angular-fire

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
+declare var moment: any;
+
 @Component({
   selector: 'side-nav',
   templateUrl: './side-nav.component.html',
@@ -10,7 +12,24 @@ export class SideNavComponent implements OnInit {
   items: FirebaseListObservable<any[]>;
 
   constructor(af: AngularFire) {
-    this.items = af.database.list('/videos');
+    //Adding videos in list
+
+    // var video = {
+    //   dateAdded: moment().format(),
+    //   url: 'testUrl',
+    //   title: 'Funny',
+    //   description: 'funny video',
+    //   primaryTag: 'primaryTag',
+    //   secondaryTag: 'secondaryTag',
+    //   likes: 0,
+    //   dislikes: 0,
+    //   comments: [{
+    //     videoID: 'testUrl'
+    //   }]
+    // };
+    //
+    // this.items = af.database.list('/videos');
+    // this.items.push(video);
   }
 
   ngOnInit() {
